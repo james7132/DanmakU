da1bb63efdbc618602772d71152988257a46f06d
james7132 <jliu348@gatech.edu>
2015-03-11 09:42:20 -0400

    Update to doc builder

--- build_doc.sh
+++ build_doc.sh
@@ -1,5 +1,9 @@
 git push origin master
 doxygen Docs/Doxyfile
 cd Docs/html
-git push origin gh-pages
+git checkout gh-pages
+git add --all
+git commit
+git push gh-pages
+git checkout master
 cd ../..
