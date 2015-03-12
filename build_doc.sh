5ca67e22049ac151f8853a3176f20af267094f27
james7132 <jliu348@gatech.edu>
2015-03-11 20:39:15 -0400

    Test 2 Document Push

--- build_doc.sh
+++ build_doc.sh
@@ -5,3 +5,8 @@ mv Docs/html ../~temp
 git checkout gh-pages
 rm -r --force Docs/html
 mv ../~temp/html Docs
+rm ../temp
+git add --all
+git commit --message="Documentation Update"
+git push
+git checkout master
