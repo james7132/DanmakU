e6abd367af3c883bfa1de05415bc6ebb3169933d
james7132 <jliu348@gatech.edu>
2015-03-11 20:55:50 -0400

    Test 3 documentation pusher

--- build_doc.sh
+++ build_doc.sh
@@ -1,12 +1,12 @@
-git push origin master
-doxygen Docs/Doxyfile
-mkdir ../~temp
-mv Docs/html ../~temp
-git checkout gh-pages
-rm -r --force Docs/html
-mv ../~temp/html Docs
-rm ../temp
-git add --all
-git commit --message="Documentation Update"
-git push
-git checkout master
+git push origin master >> ../doc_log.sh
+doxygen Docs/Doxyfile >> ../doc_log.sh
+mkdir ../~temp >> ../doc_log.sh
+mv Docs/html ../~temp >> ../doc_log.sh
+git checkout gh-pages >> ../doc_log.sh
+rm -r --force Docs/html >> ../doc_log.sh
+mv ../~temp/html Docs >> ../doc_log.sh
+rm ../~temp >> ../doc_log.sh
+git add --all >> ../doc_log.sh
+git commit --message="Documentation Update" >> ../doc_log.sh
+git push >> ../doc_log.sh
+git checkout master >> ../doc_log.sh
