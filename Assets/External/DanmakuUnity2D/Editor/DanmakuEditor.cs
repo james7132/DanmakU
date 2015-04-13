381856db49b8054b7e555c89ae448e0c17d7e915
james713 <contact@jamessliu.com>
2015-04-12 23:30:13 -0400

    Created DanmakuEditor

---
+++ Assets/External/DanmakuUnity2D/Editor/DanmakuEditor.cs
@@ -0,0 +1,188 @@
+﻿// Copyright (C) 2015  James Liu
+//	
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//		
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//			
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+using UnityEngine;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityUtilLib;
+using System.Linq;
+
+namespace Danmaku2D.Editor {
+	
+	public class DanmakuEditor : EditorWindow {
+
+		private static DanmakuEditor instance;
+		private bool test;
+		private bool selectionLock;
+		private GameObject targetGameObject;
+		private Transform targetTransform;
+		private DanmakuNoScriptContainer target;
+
+		private Vector2 scroll;
+
+		public static bool IsOpen {
+			get {
+				return instance == null;
+			}
+		}
+
+		public static void Open() {
+			if (instance == null)
+				CreateWindow ();
+			else
+				instance.Show ();
+		}
+
+		[MenuItem("Window/Danmaku Editor")]
+		public static void CreateWindow() {
+			instance = EditorWindow.GetWindow<DanmakuEditor> ("Danmaku", true);
+			instance.Show ();
+		}
+
+		void OnGUI() {
+			ToolbarGUI ();
+			BeginWindows ();
+			EditorGUILayout.BeginHorizontal ();
+			GraphGUI ();
+//			GUILayout.FlexibleSpace ();
+			EditorGUILayout.EndHorizontal ();
+			EndWindows ();
+		}
+
+		void Update() {
+			UpdateValues ();
+		}
+
+		void OnSelectionChange()  {
+			UpdateValues ();
+		}
+
+		void UpdateValues() {
+			if (!selectionLock) {
+				targetGameObject = Selection.activeGameObject;
+				if (targetGameObject != null) {
+					target = targetGameObject.GetComponentInChildren<DanmakuNoScriptContainer> ();
+					targetTransform = targetGameObject.transform;
+				} else {
+					target = null;
+					targetTransform = null;
+				}
+			}
+			Repaint ();
+		}
+		
+		void ToolbarGUI() {
+			GUILayout.BeginHorizontal (EditorStyles.toolbar);
+			if (target != null) {
+				EditorGUILayout.LabelField("Selected: " + target.name);
+			}
+			GUILayout.FlexibleSpace ();
+			selectionLock = GUILayout.Toggle (selectionLock, "Lock", EditorStyles.toolbarButton);
+			GUILayout.EndHorizontal ();
+		}
+
+		void SelectionGUI() {
+//			EditorGUILayout.BeginVertical (GUILayout.Width(selectionWidth));
+//			{
+//				selectionScroll = EditorGUILayout.BeginScrollView (selectionScroll);
+//				{
+////					foreach(GameObject gameObj in targets) {
+////						EditorGUILayout.LabelField(gameObj.name);
+////					}
+////					GUILayout.FlexibleSpace();
+//				}
+//				EditorGUILayout.EndScrollView ();
+//			}
+//			EditorGUILayout.EndVertical ();
+//			Rect temp = GUILayoutUtility.GetLastRect ();
+//
+//			Event currentEvent = Event.current;
+//			EventType currentEventType = currentEvent.type;
+//
+//			if (currentEventType == EventType.DragExited) {
+//				DragAndDrop.PrepareStartDrag();
+//			}
+//
+//			if (temp.Contains (currentEvent.mousePosition)) {
+//				
+////				switch (currentEventType) {
+////					case EventType.DragUpdated:
+////						Object[] objects = DragAndDrop.objectReferences;
+////						if(objects.Length > 0) {
+////							foreach(Object obj in objects) {
+////								if(obj is GameObject && !targets.Contains(obj as GameObject)) {
+////									DragAndDrop.visualMode = DragAndDropVisualMode.Link;
+////									break;
+////								}
+////							}
+////						}
+////						break;
+////					case EventType.DragPerform:
+////						foreach(GameObject gameObject in DragAndDrop.objectReferences) {
+////							AddToSelection(gameObject);
+////						}
+////						break;
+////					default:
+////						break;
+////				}
+//			}
+		}
+
+		void GraphGUI() {
+			Rect temp = EditorGUILayout.BeginVertical ();
+			{
+				scroll = EditorGUILayout.BeginScrollView (scroll, (GUIStyle)"PreBackground");
+				{
+					GUILayout.FlexibleSpace ();
+				}
+				EditorGUILayout.EndScrollView ();
+			}
+			EditorGUILayout.EndVertical ();
+			
+			Event currentEvent = Event.current;
+			EventType currentEventType = currentEvent.type;
+
+			if (currentEventType == EventType.DragExited) {
+				DragAndDrop.PrepareStartDrag();
+			}
+
+			if (temp.Contains (currentEvent.mousePosition)) {
+				
+				switch (currentEventType) {
+					case EventType.DragUpdated:
+						Object[] objects = DragAndDrop.objectReferences;
+						if (objects.Length > 0) {
+							foreach (Object obj in objects) {
+								
+							}
+						}
+						break;
+					case EventType.DragPerform:
+						foreach (GameObject gameObject in DragAndDrop.objectReferences) {
+
+						}
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		void DragAndDropCheck() {
+		}
+	}
+
+}
---
+++ Assets/External/DanmakuUnity2D/NoScript/DanmakuNoScriptContainer.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace Danmaku2D {
+
+	[ExecuteInEditMode]
+	[DisallowMultipleComponent]
+	public class DanmakuNoScriptContainer : MonoBehaviour {
+
+		[SerializeField]
+		private DanmakuTrigger[] triggers;
+
+		[SerializeField]
+		private List<Source> sources;
+
+		private Dictionary<Guid, Source> sourceMap;
+
+		[System.Serializable]
+		private class Source : ISerializationCallbackReceiver {
+			public string name;
+			public Transform transform;
+			[SerializeField]
+			private string identificationString;
+			public Guid identifier;
+
+			public Source(Transform transform) { 
+				this.transform = transform;
+				name = transform.name;
+				RegenerateID();
+			}
+
+			public void RegenerateID() {
+				identifier = Guid.NewGuid();
+			}
+
+			#region ISerializationCallbackReceiver implementation
+
+			public void OnBeforeSerialize () {
+				identificationString = identifier.ToString ();
+			}
+
+			public void OnAfterDeserialize () {
+				if (identificationString == null)
+					RegenerateID ();
+				else 
+					identifier = new Guid (identificationString);
+			}
+
+			#endregion
+		}
+
+		void OnEnable() {
+			sourceMap = new Dictionary<Guid, Source> ();
+			for (int i = 0; i < sources.Count; i++) {
+				sourceMap[sources[i].identifier] = sources[i];
+			}
+		}
+
+		#if UNITY_EDITOR
+		void Update() {
+			if (Application.isEditor) {
+				triggers = GetComponentsInChildren<DanmakuTrigger>();
+				for(int i = 0; i < sources.Count; i++) {
+					if(sources[i].identifier == Guid.Empty)
+						sources[i].RegenerateID();
+				}
+			}
+			DanmakuNoScriptContainer[] containers = GetComponentsInChildren<DanmakuNoScriptContainer>();
+			foreach(var container in containers) {
+				if(container != this)
+					DestroyImmediate(container);
+			}
+		}
+		#endif
+	}
+
+}
