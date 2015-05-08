3bd5a09fc6dc3ab2e7199c311bab5b79fa4b9ad6
james713 <contact@jamessliu.com>
2015-05-07 23:23:21 -0400

    Reorganization Fixes

--- Core/Controllers/FireDanmakuController.cs
+++ Core/Controllers/FireDanmakuController.cs
@@ -1,31 +1,31 @@
-// Copyright (c) 2015 James Liu
-//	
-// See the LISCENSE file for copying permission.
-
-using UnityEngine;
-
-namespace DanmakU.DanmakuControllers {
-
-	[System.Serializable]
-	public class FireDanmakuController : IDanmakuController {
-
-		[SerializeField]
-		private float delay;
-
-		[SerializeField]
-		private bool repeat;
-
-		[SerializeField]
-		private bool deactivateAfterwards;
-
-		[SerializeField]
-		private DanmakuEmitter emitter;
-
-		#region IDanmakuController implementation
-		public void UpdateDanmaku (Danmaku danmaku, float dt) {
-			//TODO: IMPLEMENT
-		}
-		#endregion
-		
-	}
-}
+//// Copyright (c) 2015 James Liu
+////	
+//// See the LISCENSE file for copying permission.
+//
+//using UnityEngine;
+//
+//namespace DanmakU.DanmakuControllers {
+//
+//	[System.Serializable]
+//	public class FireDanmakuController : IDanmakuController {
+//
+//		[SerializeField]
+//		private float delay;
+//
+//		[SerializeField]
+//		private bool repeat;
+//
+//		[SerializeField]
+//		private bool deactivateAfterwards;
+//
+//		[SerializeField]
+//		//private DanmakuEmitter emitter;
+//
+//		#region IDanmakuController implementation
+//		public void UpdateDanmaku (Danmaku danmaku, float dt) {
+//			//TODO: IMPLEMENT
+//		}
+//		#endregion
+//		
+//	}
+//}
--- build_doc.sh
+++
@@ -1,4 +0,0 @@
-git push origin master
-doxygen Docs/Doxyfile
-cd html
-git push origin gh-pages
