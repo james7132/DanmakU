611d1232016bec1e15a72de095df4037eddf4425
james7132 <james.siqi.liu@gmail.com>
2015-05-18 02:46:10 -0400

    FireBuilder Additions
    Added more FireBuilder functions for controlling position and rotation.
    All of them use the fluent interface design.
    
    Also changed SpeedLimitController to act using minimums and maximums
    instead of the enums that were used before.

--- Assets/DanmakU/Core/FireBuilder.cs
+++ Assets/DanmakU/Core/FireBuilder.cs
@@ -45,36 +45,189 @@ namespace DanmakU {
 			}
 		}
 
+		private DanmakuField field;
+		public DanmakuField Field {
+			get {
+				return field;
+			}
+			set {
+				field = value;
+			}
+		}
+
+		private Vector2 position;
+		public Vector2 Position {
+			get {
+				return position;
+			}
+			set {
+				position = value;
+			}
+		}
+
+		private DynamicFloat rotation;
+		public DynamicFloat Rotation {
+			get {
+				return rotation;
+			}
+			set {
+				rotation = value;
+			}
+		}
+
+		private DanmakuController controller;
+		public event DanmakuController Controller {
+			add {
+				controller += value;
+			}
+			remove {
+				controller -= value;
+			}
+		}
+
 		private Stack<DanmakuModifier> modifiers;
 		
-		public FireBuilder(DanmakuPrefab prefab) {
-
+		public FireBuilder(DanmakuPrefab prefab, DanmakuField field = null) {
 			this.prefab = prefab;
+			this.field = field;
 			modifiers = new Stack<DanmakuModifier>();
 		}
 
-		public FireBuilder AddModifier(DanmakuModifier modifier) {
+		public FireBuilder At (Vector2 position) {
+			this.position = position;
+			return this;
+		}
+
+		public FireBuilder At (Transform transform) {
+			if(transform == null)
+				throw new System.ArgumentNullException();
+
+			position = transform.position;
+			return this;
+		}
+
+		public FireBuilder At (GameObject gameObject) {
+			if(gameObject == null)
+				throw new System.ArgumentNullException();
+			
+			position = gameObject.transform.position;
+			return this;
+		}
+
+		public FireBuilder At (Component component) {
+			if(component == null)
+				throw new System.ArgumentNullException();
+			
+			position = component.transform.position;
+			return this;
+		}
+
+		public FireBuilder At (Vector2 position, 
+		                       DanmakuField field, 
+		                       DanmakuField.CoordinateSystem coordSys = DanmakuField.CoordinateSystem.View) {
+			if(field == null)
+				throw new System.ArgumentNullException();
+
+			this.field = field;
+			this.position = field.WorldPoint(position, coordSys);
+			return this;
+		}
+
+		public FireBuilder Rotate(DynamicFloat rotation) {
+			this.rotation = rotation;
+			return this;
+		}
+		
+		public FireBuilder Rotate (Transform transform) {
+			if(transform == null)
+				throw new System.ArgumentNullException();
+			
+			rotation = transform.Rotation2D();
+			return this;
+		}
+		
+		public FireBuilder Rotate (GameObject gameObject) {
+			if(gameObject == null)
+				throw new System.ArgumentNullException();
+			
+			rotation = gameObject.transform.Rotation2D();
+			return this;
+		}
+		
+		public FireBuilder Rotate (Component component) {
+			if(component == null)
+				throw new System.ArgumentNullException();
+			
+			rotation = component.transform.Rotation2D();
+			return this;
+		}
+
+		public FireBuilder Transform (Transform transform) {
+			return At (transform).Rotate (transform);
+		}
+
+		public FireBuilder Transform (GameObject gameObject) {
+			if(gameObject == null)
+				throw new System.ArgumentNullException();
+
+			return Transform (gameObject.transform);
+		}
+
+		public FireBuilder Transform (Component component) {
+			if(component == null)
+				throw new System.ArgumentNullException();
+
+			return Transform (component.transform);
+		}
+
+		public FireBuilder WithModifier(DanmakuModifier modifier) {
 			modifiers.Push(modifier);
 			return this;
 		}
 
 		public FireBuilder WithController (IDanmakuController controller) {
-			modifiers.Push(new AddControllersModifier(controller));
+			this.controller += controller.Update;
 			return this;
 		}
 
 		public FireBuilder WithController (IEnumerable<IDanmakuController> controllers) {
-			modifiers.Push(new AddControllersModifier(controllers));
+			var list = controllers as IList<IDanmakuController>;
+			if(list != null) {
+				int count = list.Count;
+				for(int i = 0; i < count; i++) {
+					IDanmakuController current = list[i];
+					if(current != null)
+						this.controller += current.Update;
+				}
+			} else {
+				foreach(var controller in controllers) {
+					if(controller != null)
+						this.controller += controller.Update;
+				}
+			}
 			return this;
 		}
 
 		public FireBuilder WithController (DanmakuController controller) {
-			modifiers.Push(new AddControllersModifier(controller));
+			this.controller += controller;
 			return this;
 		}
 
 		public FireBuilder WithController (IEnumerable<DanmakuController> controllers) {
-			modifiers.Push(new AddControllersModifier(controllers));
+			var list = controllers as IList<DanmakuController>;
+			if(list != null) {
+				int count = list.Count;
+				for(int i = 0; i < count; i++) {
+					DanmakuController current = list[i];
+					if(current != null)
+						this.controller += current;
+				}
+			} else {
+				foreach(var controller in controllers) {
+					if(controller != null)
+						this.controller += controller;
+				}
+			}
 			return this;
 		}
 
@@ -85,13 +238,12 @@ namespace DanmakU {
 
 		public void Fire() {
 			DanmakuModifier modifier = DanmakuModifier.Construct(modifiers);
-			modifier.Initialize(new FireData() { Prefab = Prefab });
-			modifier.OnFire(Vector2.zero, 0f);
+			modifier.Initialize(new FireData() { 
+				Prefab = prefab,
+				Controller = controller
+			});
+			modifier.OnFire(position, rotation);
 		}
-
-		public void Fire(FireData data) {
-		}
-		
 	}
 }
 
