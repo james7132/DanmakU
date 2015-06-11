16b4ebd88e5626b8a8f882e07565842e458053a1
james7132 <jliu348@gatech.edu>
2015-06-11 09:25:22 -0400

    DanmakuBehavior Additions
    More utility functions for Coroutines and Tasks

--- Assets/Plugins/DanmakU/Core/DanmakuBehavior.cs
+++ Assets/Plugins/DanmakU/Core/DanmakuBehavior.cs
@@ -10,6 +10,44 @@ namespace DanmakU {
 
     public abstract class DanmakuBehaviour : BetterBehaviour {
 
+		static DanmakuBehaviour() {
+			WaitForEndOfFrame = new UnityEngine.WaitForEndOfFrame();
+		}
+
+		protected static readonly YieldInstruction WaitForEndOfFrame;
+
+		protected static readonly YieldInstruction WaitForNextFrame = null;
+
+		private IEnumerator waitForFrames(int frameCount) {
+			int current = 0;
+			while(current < frameCount) {
+				if(enabled)
+					current++;
+				yield return null;
+			}
+		}
+
+		private IEnumerator waitForSeconds(float seconds) {
+			float current = 0;
+			while(current < seconds) {
+				if(enabled)
+					current += Dt;
+				yield return null;
+			}
+		}
+
+		protected YieldInstruction WaitForFrames(int frameCount) {
+			if(frameCount < 0)
+				return null;
+			return StartCoroutine(waitForFrames(frameCount));
+		}
+
+		protected YieldInstruction WaitForSeconds(float seconds) {
+			if(seconds < Dt)
+				return null;
+			return StartCoroutine(waitForSeconds(seconds));
+		}
+
         /// <summary>
         /// Shorthand for <c>TimeUtil.DeltaTime</c>.
         /// </summary>
@@ -44,7 +82,6 @@ namespace DanmakU {
                 throw new System.ArgumentNullException("task");
             return new Task(this, task);
         }
-
     }
 
 }
