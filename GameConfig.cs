0ce1ad9cc3badb048d41e6098f3725a9fce35f13
james7132 <james.siqi.liu@gmail.com>
2015-08-19 16:15:38 -0400

    Seperated out a Generalized Game and GameConfig
    Allowed for reuse elsewhere via HouraiLib. SmashBrew will have the derived
    classes SmashGame and SmashConfig accordingly.

---
+++ GameConfig.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vexe.Runtime.Types;
+
+namespace Hourai {
+
+    [DefineCategories("Tags")]
+    public abstract class GameConfig : BaseScriptableObject {
+        
+        [Serialize, Show, Tags, Category("Tags"), Default("Player")]
+        private string _playerTag;
+
+        public string PlayerTag {
+            get { return _playerTag; }
+        }
+
+        [Serialize, Show, Tags, Category("Tags"), Default("Respawn")]
+        private string _respawnTag;
+
+        public string RespawnTag {
+            get { return _respawnTag; }
+        }
+    }
+
+}
+
