9e600735f920fe872314e1d86a3ebaf7865ef433
james7132 <james.siqi.liu@gmail.com>
2015-08-23 03:28:07 -0400

    Preliminary In-Match Player Data Display

---
+++ UI/MatchColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hourai {
+
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Graphic))]
+    public class MatchColor : MonoBehaviour {
+
+        [SerializeField]
+        private Graphic target;
+
+        private Graphic _self;
+
+        // Update is called once per frame
+        void Update() {
+            if (target == null)
+                return;
+
+            if (_self == null)
+                _self = GetComponent<Graphic>();
+
+            _self.color = target.color;
+        }
+    }
+
+}
