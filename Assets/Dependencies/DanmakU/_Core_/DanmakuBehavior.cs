2030e46460277b85d2dac9972c6b15c6ce05b992
james7132 <james.siqi.liu@gmail.com>
2015-09-03 21:56:07 -0400

    Removed FastTrig
    It wasn't fast, in fact it was slower by almost a factor of 10.

--- Assets/Dependencies/DanmakU/_Core_/DanmakuBehavior.cs
+++ Assets/Dependencies/DanmakU/_Core_/DanmakuBehavior.cs
@@ -16,29 +16,6 @@ namespace Hourai.DanmakU {
         protected static float Dt {
             get { return TimeUtil.DeltaTime; }
         }
-
-		/// <summary>
-		/// Calculates the cosine of the 
-		/// </summary>
-		/// <remarks>
-		/// This function has much better performance than Mathf.Cos; however it can have much lower accuracy.
-		/// </remarks>
-		/// <param name="degree">Degree.</param>
-        protected static float Cos(float degree) {
-            return FastTrig.Cos(degree);
-        }
-
-        protected static float Sin(float degree) {
-            return FastTrig.Sin(degree);
-        }
-
-        protected static float Tan(float degree) {
-            return FastTrig.Tan(degree);
-        }
-
-        protected static Vector2 UnitCircle(float degree) {
-            return FastTrig.UnitCircle(degree);
-        }
     }
 
 }
