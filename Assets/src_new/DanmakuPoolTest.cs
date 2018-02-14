f875a1a552eb146d858883d13624e695279615c7
james713 <contact@jamessliu.com>
2018-02-14 05:12:51 -0800

    Begin C# Jobs implementation of DanmakU

---
+++ Assets/src_new/DanmakuPoolTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanmakuPoolTest : MonoBehaviour {
+
+  DanmakuPool pool;
+  Danmaku danmakuTest;
+  Transform[] objects;
+
+  public int PoolSize;
+  int counter;
+
+	void Start () {
+    objects = new Transform[PoolSize];
+    pool = new DanmakuPool(PoolSize);
+    for (int i = 0; i < PoolSize; i++) {
+      var danmaku = pool.Get();
+      danmaku.Position = 40 * Random.insideUnitCircle;
+      danmaku.Speed = Random.value;
+      danmaku.Rotation = Random.value * Mathf.PI * 2;
+      danmaku.AngularSpeed = Random.Range(-1f, 1f) * Mathf.PI / 128;
+      objects[i] = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+      objects[i].GetComponent<Renderer>().material.color = new Color(Random.value,Random.value, Random.value);
+    }
+	}
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update() {
+    pool.StartUpdate();
+    for (int i = 0; i < objects.Length; i++) {
+      objects[i].localPosition = pool.Positions[i];
+      objects[i].localRotation = Quaternion.Euler(0, 0, pool.Rotations[i] * Mathf.Rad2Deg);
+    }
+  }
+
+  /// <summary>
+  /// This function is called when the MonoBehaviour will be destroyed.
+  /// </summary>
+  void OnDestroy() => pool.Dispose();
+
+}
