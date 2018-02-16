000e78d35c3d49f78fbe37febdb0f7c63c85001e
james7132 <james.siqi.liu@gmail.com>
2018-02-16 14:39:03 -0800

    Make Shape firing no-alloc

--- Assets/src/Fireables/Shapes/Circle.cs
+++ Assets/src/Fireables/Shapes/Circle.cs
@@ -5,43 +5,42 @@ using UnityEngine;
 
 namespace DanmakU.Fireables {
 
-    [Serializable]
-    public class Circle : ShapeFireable {
+[Serializable]
+public class Circle : Fireable {
 
-        [SerializeField]
-        Range _count;
+  [SerializeField]
+  Range _count;
 
-        [SerializeField]
-        Range _radius;
+  [SerializeField]
+  Range _radius;
 
-        public Range Count { 
-            get { return _count;} 
-            set { _count = value; } 
-        }
+  public Range Count { 
+      get { return _count;} 
+      set { _count = value; } 
+  }
 
-        public Range Radius { 
-            get { return _radius;} 
-            set { _radius = value; } 
-        }
+  public Range Radius { 
+      get { return _radius;} 
+      set { _radius = value; } 
+  }
 
-        public Circle(Range count, Range radius) {
-            Count = count;
-            Radius = radius;
-        }
+  public Circle(Range count, Range radius) {
+      Count = count;
+      Radius = radius;
+  }
 
-        protected override IEnumerable<DanmakuState> GetSubemissions(DanmakuState state) {
-            float radius = Radius.GetValue();
-            int count = Mathf.RoundToInt(Count.GetValue());
-            var rotation = state.Rotation.GetValue();
-            for (int i = 0; i < count; i++) {
-                var currentState = state;
-                var angle = rotation + i * (Mathf.PI * 2 / count);
-                state.Position = state.Position + (radius * RotationUtil.ToUnitVector(angle));
-                state.Rotation = rotation;
-                yield return currentState;
-            }
-        }
-    }
+  public override void Fire(DanmakuState state) {
+      float radius = Radius.GetValue();
+      int count = Mathf.RoundToInt(Count.GetValue());
+      var rotation = state.Rotation.GetValue();
+      for (int i = 0; i < count; i++) {
+        var angle = rotation + i * (Mathf.PI * 2 / count);
+        state.Position = state.Position + (radius * RotationUtil.ToUnitVector(angle));
+        state.Rotation = rotation;
+        Subfire(state);
+      }
+  }
+}
 
 }
 
--- Assets/src/Fireables/Shapes/Ring.cs
+++ Assets/src/Fireables/Shapes/Ring.cs
@@ -5,43 +5,30 @@ using UnityEngine;
 
 namespace DanmakU.Fireables {
 
-    [Serializable]
-    public class Ring : ShapeFireable {
+[Serializable]
+public class Ring : Fireable {
 
-        [SerializeField]
-        Range _count;
+  public Range Count;
+  public Range Radius;
 
-        [SerializeField]
-        Range _radius;
+  public Ring(Range count, Range radius) {
+    Count = count;
+    Radius = radius;
+  }
 
-        public Range Count { 
-            get { return _count;} 
-            set { _count = value; } 
-        }
-
-        public Range Radius { 
-            get { return _radius;} 
-            set { _radius = value; } 
-        }
-
-        public Ring(Range count, Range radius) {
-            Count = count;
-            Radius = radius;
-        }
-
-        protected override IEnumerable<DanmakuState> GetSubemissions(DanmakuState state) {
-            float radius = Radius.GetValue();
-            int count = Mathf.RoundToInt(Count.GetValue());
-            var rotation = state.Rotation.GetValue();
-            for (int i = 0; i < count; i++) {
-                var currentState = state;
-                var angle = rotation + i * (Mathf.PI * 2 / count);
-                currentState.Position = state.Position + (radius * RotationUtil.ToUnitVector(angle));
-                currentState.Rotation = angle;
-                yield return currentState;
-            }
-        }
+  public override void Fire(DanmakuState state) {
+    float radius = Radius.GetValue();
+    int count = Mathf.RoundToInt(Count.GetValue());
+    var rotation = state.Rotation.GetValue();
+    var currentState = state;
+    for (int i = 0; i < count; i++) {
+      var angle = rotation + i * (Mathf.PI * 2 / count);
+      currentState.Position = state.Position + (radius * RotationUtil.ToUnitVector(angle));
+      currentState.Rotation = angle;
+      Subfire(currentState);
     }
+  }
+}
 
 }
 
