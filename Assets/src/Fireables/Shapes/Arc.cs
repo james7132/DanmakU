9b9d81b45e78c4fa6976e7378eac1554da635679
james713 <contact@jamessliu.com>
2018-02-16 14:39:03 -0800

    Make Shape firing no-alloc

--- Assets/src/Fireables/Shapes/Arc.cs
+++ Assets/src/Fireables/Shapes/Arc.cs
@@ -4,52 +4,33 @@ using UnityEngine;
 
 namespace DanmakU.Fireables {
 
-    [Serializable]
-    public class Arc : ShapeFireable {
+[Serializable]
+public class Arc : Fireable {
 
-        [SerializeField]
-        Range _count;
-        [SerializeField]
-        Range _arcLength;
-        [SerializeField]
-        Range _radius;
+  public Range Count;
+  public Range ArcLength;
+  public Range Radius;
 
-        public Range Count { 
-            get { return _count;} 
-            set { _count = value; } 
-        }
+  public Arc(Range count, Range arcLength, Range radius) {
+    Count = count;
+    ArcLength = arcLength;
+    Radius = radius;
+  }
 
-        public Range ArcLength { 
-            get { return _arcLength;} 
-            set { _arcLength = value; } 
-        }
-
-        public Range Radius { 
-            get { return _radius;} 
-            set { _radius = value; } 
-        }
-
-        public Arc(Range count, Range arcLength, Range radius) {
-            Count = count;
-            ArcLength = arcLength;
-            Radius = radius;
-        }
-
-        protected override IEnumerable<DanmakuState> GetSubemissions(DanmakuState state) {
-            float radius = Radius.GetValue();
-            int count = Mathf.RoundToInt(Count.GetValue());
-            float arcLength = ArcLength.GetValue();
-            var rotation = state.Rotation.GetValue();
-            var start = rotation - arcLength / 2;
-            for (int i = 0; i < count; i++) {
-                var currentState = state;
-                var angle = start + i * (arcLength / count);
-                state.Position = state.Position + 
-                    (radius * RotationUtil.ToUnitVector(angle));
-                state.Rotation = angle;
-                yield return currentState;
-            }
-        }
+  public override void Fire(DanmakuState state) {
+    float radius = Radius.GetValue();
+    int count = Mathf.RoundToInt(Count.GetValue());
+    float arcLength = ArcLength.GetValue();
+    var rotation = state.Rotation.GetValue();
+    var start = rotation - arcLength / 2;
+    var currentState = state;
+    for (int i = 0; i < count; i++) {
+      var angle = start + i * (arcLength / count);
+      state.Position = state.Position + (radius * RotationUtil.ToUnitVector(angle));
+      state.Rotation = angle;
+      Subfire(currentState);
     }
+  }
+}
 
 }
