337b1a46993b2771272d19970c0edb3ef01cb3bb
james7132 <james.siqi.liu@gmail.com>
2018-02-19 05:42:16 -0800

    Use UnsafeUtility.MemCpy to fill buffers.

--- Assets/DanmakuAcceleration.cs
+++ Assets/DanmakuAcceleration.cs
@@ -10,6 +10,8 @@ public class DanmakuAcceleration : MonoBehaviour, IDanmakuModifier {
   public float Acceleration;
 
   public JobHandle UpdateDannmaku(DanmakuPool pool, JobHandle dependency = default(JobHandle)) {
+    var acceleration = Acceleration * Time.deltaTime;
+    if (Mathf.Approximately(acceleration, 0f)) return dependency;
     return new ApplyAcceleration {
       Acceleration = Acceleration * Time.deltaTime,
       Speeds = pool.Speeds
