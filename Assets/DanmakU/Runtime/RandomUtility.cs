39ea5bcb3c3aca50935ec49faf07d3f346d8e052
james7132 <james.siqi.liu@gmail.com>
2018-02-19 22:01:22 -0800

    Support Range evaluation in Jobs

---
+++ Assets/DanmakU/Runtime/RandomUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace DanmakU {
+
+internal static class RandomUtility {
+
+  [ThreadStatic] static Random Random = new Random();
+
+  public static float Range(float min, float max) {
+    var random = Random ?? (Random = new Random());
+    return Mathf.Lerp(min, max, (float)random.NextDouble());
+  }
+
+}
+
+}
