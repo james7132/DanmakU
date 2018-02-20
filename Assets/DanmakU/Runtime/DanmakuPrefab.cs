ebfb8305d3537265dad7025ac996e4aa469e4de1
james713 <contact@jamessliu.com>
2018-02-19 20:33:34 -0800

    Add more documentation

--- Assets/DanmakU/Runtime/DanmakuPrefab.cs
+++ Assets/DanmakU/Runtime/DanmakuPrefab.cs
@@ -13,6 +13,40 @@ internal struct DanmakuRendererConfig {
   public Material Material;
 }
 
+/// <summary>
+/// A prototype base for <see cref="DanmakU.Danmaku"/>. A metaphorical "prefab" for creating
+/// instances of <see cref="DanmakU.DanmakuSet"/>.
+/// </summary>
+/// <remarks>
+/// <para>
+/// Implemented as a MonoBehaviour. This can be attached to GameObject prefabs denote it is a base for
+/// making. Any attached Components that inherit from <see cref="DanmakU.IDanmakuModifier"/> will be added as
+/// modifiers to any <see cref="DanmakU.DanmakuSet"/> created from the prefab.
+/// </para>
+/// </remarks>
+/// <example>
+/// In scripts, the type DanmakuPrefab is mostly used as a serializable reference to a base for creating 
+/// bullet types. This allows designers to change out the look and behaviour of bullet patterns without needing 
+/// to modify the code.
+/// <code>
+/// public class CustomDanmakuPattern : DanmakuBehaviour {
+///   // Serialize a reference to a DanmakuPrefab.
+///   public DanmakuPrefab Prefab;
+///   IFireable fireable;
+/// 
+///   void Start() {
+///     // Create a DanmakuSet from the prefab.
+///     fireable = CreateSet(Prefab);
+///   }
+///   
+///   void Update() {
+///     // Fire the bullets!
+///     fireable.Fire();
+///   }
+/// }
+/// </code>
+/// </example>
+[AddComponentMenu("DanmakU/Danmaku Prefab")]
 public class DanmakuPrefab : MonoBehaviour {
 
   enum RendererType { Sprite, Mesh }
@@ -38,7 +72,7 @@ public class DanmakuPrefab : MonoBehaviour {
     Gizmos.DrawWireSphere(center, ColliderRadius);
   }
 
-  public IDanmakuModifier[] GetModifiers() {
+  internal IDanmakuModifier[] GetModifiers() {
     return Modifiers ?? (Modifiers = GetComponents<IDanmakuModifier>());
   }
 
--- Assets/DanmakU/Runtime/Range.cs
+++ Assets/DanmakU/Runtime/Range.cs
@@ -2,56 +2,122 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DanmakU {
 
-	[Serializable]
-	public struct Range {
+/// <summary>
+/// A value type that represents a range of floating point values.
+/// </summary>
+/// <example>
+/// Ranges are implicitly convertible to and from floats and support most basic 
+/// arithmetic with them.
+/// <code>
+/// Range r1 = 5f;                  // Converted to a range of [5, 5]
+/// Range r2 = r1.Expanded(5);      // Expanded range to [0, 10]
+/// Range r3 = r2 + 4;              // Shifted range to [4, 14]
+/// Range r4 = r3 - 9;              // Shifted range to [-5, 5]
+/// Range r5 = r4 * 20;             // Multiplied range to [-100, 100]
+/// Range r6 = r5 / 10;             // Divided range to [-10, 10]
+/// </code>
+/// </example>
+[Serializable]
+public struct Range {
 
-		public float Min;
-		public float Max;
+  [SerializeField] float _min;
+  [SerializeField] float _max;
 
-		public Range(float val) : this(val, val) {
-		}
+  /// <summary>
+  /// Gets the smallest value in the range.
+  /// </summary>
+  public float Min => _min;
 
-		public Range(float min, float max) {
-			if (min > max) {
-				Min = max;
-				Max = min;
-			} else {
-				Max = max;
-				Min = min;
-			}
-		}
+  /// <summary>
+  /// Gets the largest value in the range.
+  /// </summary>
+  public float Max => _max;
 
-		public float GetValue() {
-			return UnityEngine.Random.value * (Max - Min) + Min;
-		}
+  /// <summary>
+  /// Gets the midpoint value of the range.
+  /// </summary>
+  public float Center => (Max + Min) / 2f;
 
-		public static implicit operator float(Range range) {
-			return range.GetValue();
-		}
+  /// <summary>
+  /// Gets the size of the range.
+  /// </summary>
+  public float Size => Max - Min;
 
-		public static implicit operator Range(float val) {
-			return new Range(val);
-		}
+  /// <summary>
+  /// Creates a range scoped to the value of only one value.
+  /// </summary>
+  /// <param name="val">the center value of the range</param>
+  /// <example>
+  /// <code>
+  /// new Range(0)         // A range of [0, 0]
+  /// new Range(5)         // A range of [5, 5]
+  /// new Range(1000, 100) // A range of [100, 100]
+  /// </code>
+  /// </example>
+  public Range(float val) : this(val, val) {}
 
-		public static Range operator +(Range lhs, Range rhs) {
-			return new Range(lhs.Min + rhs.Min, lhs.Max + rhs.Max);
-		}
+  /// <summary>
+  /// Creates a range based on an interval of values.
+  /// </summary>
+  /// <param name="a">one extrema of the range.</param>
+  /// <param name="b">one extrema of the range.</param>  
+  /// <example>
+  /// <code>
+  /// new Range(0, 5)   // A range of [0, 5]
+  /// new Range(5, 0)   // Another way to express [0, 5]
+  /// new Range(5, 100) // A range of [5, 100]
+  /// new Range(100, 5) // Another way to express of [5, 100]
+  /// </code>
+  /// </example>
+  public Range(float a, float b) {
+    if (a > b) {
+      _min = b;
+      _max = a;
+    } else {
+      _max = b;
+      _min = a;
+    }
+  }
 
-		public static Range operator -(Range lhs, Range rhs) {
-			return new Range(lhs.Min - rhs.Min, lhs.Max - rhs.Max);
-		}
+  /// <summary>
+  /// Creates a new Range expanded from the current range.
+  /// </summary>
+  /// <remarks>
+  /// Can be used to shrink the new range by providiing a negative size.
+  /// If the absolute value of <paramref cref="size"/> is larger than the 
+  /// <see cref="Size"/> of the Range, the range will 
+  /// </remarks>
+  /// <param name="size">the size</param>
+  /// <returns></returns>
+  public Range Expanded(float size) {
+    var extents = size / 2f;
+    return new Range(Min - extents, Max + extents);
+  }
 
-		public static Range operator *(Range lhs, float rhs) {
-			return new Range(lhs.Min * rhs, lhs.Max * rhs);
-		}
+  /// <summary>
+  /// Clamps a value to the limits of the range.
+  /// </summary>
+  /// <param name="value">the value to clamp.</param>
+  /// <returns>the clamped value.</returns>
+  public float Clamp(float value) => Mathf.Clamp(value, Min, Max);
 
-		public static Range operator /(Range lhs, float rhs) {
-			return new Range(lhs.Min / rhs, lhs.Max / rhs);
-		}
+  /// <summary>
+  /// Uniformily samples a value from the region.
+  /// </summary>
+  /// <returns>the randomly sampled value.</returns>
+  public float GetValue() => Random.Range(Min, Max);
 
-	}
+  public static implicit operator Range(float val) => new Range(val);
+
+  public static Range operator +(Range lhs, Range rhs) => new Range(lhs.Min + rhs.Min, lhs.Max + rhs.Max);
+  public static Range operator -(Range lhs, Range rhs) => new Range(lhs.Min - rhs.Min, lhs.Max - rhs.Max);
+  public static Range operator *(Range lhs, float rhs) => new Range(lhs.Min * rhs, lhs.Max * rhs);
+  public static Range operator /(Range lhs, float rhs) => new Range(lhs.Min / rhs, lhs.Max / rhs);
+
+}
 
 }
