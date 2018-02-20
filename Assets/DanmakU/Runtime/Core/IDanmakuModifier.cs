ba3672bc6c95c4faef585420b197ae562553841a
james7132 <james.siqi.liu@gmail.com>
2018-02-19 21:42:45 -0800

    Move Modifiers to their own Namespace

--- Assets/DanmakU/Runtime/Core/IDanmakuModifier.cs
+++ Assets/DanmakU/Runtime/Core/IDanmakuModifier.cs
@@ -2,8 +2,18 @@
 
 namespace DanmakU {
 
+/// <summary>
+/// A composable modifier to runtime behaviour of a set of bullets.
+/// </summary>
+/// <seealso cref="DanmakU.DanmakuSet"/>
 public interface IDanmakuModifier {
 
+  /// <summary>
+  /// Adds a modification Unity Job to add 
+  /// </summary>
+  /// <param name="pool"></param>
+  /// <param name="dependency"></param>
+  /// <returns></returns>
   JobHandle UpdateDannmaku(DanmakuPool pool, JobHandle dependency);
 
 }
