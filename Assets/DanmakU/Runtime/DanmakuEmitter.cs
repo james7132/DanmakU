ebbcdd29177166c8c7a0f2f1b149140a935361cc
Rafael Irgolic <rafael.irgolic@gmail.com>
2018-06-19 01:51:45 +0200

    refactor

--- Assets/DanmakU/Runtime/DanmakuEmitter.cs
+++ Assets/DanmakU/Runtime/DanmakuEmitter.cs
@@ -21,18 +21,12 @@ namespace DanmakU
         public bool isFiring { get; set; }
 
         //Editor OK   **WARNING** Do not access directly; use GetTeamNo and SetTeamNo
-        public int TeamNo;
-
-        public int GetTeamNo()
+        public int TeamNo
         {
-            return TeamNo;
+            get { return TeamNo;} 
+            set { if (value != TeamNo) set.Pool.TeamNo = TeamNo = value; }
         }
-
-        public void SetTeamNo(int newTeam)
-        {
-            if (newTeam != TeamNo)
-                set.Pool.TeamNo = TeamNo = newTeam;
-        }
+        
 
         float timer;
 
