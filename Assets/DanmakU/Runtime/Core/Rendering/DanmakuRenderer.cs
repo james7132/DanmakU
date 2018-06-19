c7d6d4f38d45c414fcc930334e9d3be59438a09c
James Liu <contact@jamessliu.com>
2018-06-19 14:24:24 -0700

    Fix #39

--- Assets/DanmakU/Runtime/Core/Rendering/DanmakuRenderer.cs
+++ Assets/DanmakU/Runtime/Core/Rendering/DanmakuRenderer.cs
@@ -73,7 +73,7 @@ internal class DanmakuRenderer : IDisposable {
 
     foreach (var set in sets) {
       var pool = set.Pool;
-      if (pool == null || pool.ActiveCount <= 0) return;
+      if (pool == null || pool.ActiveCount <= 0) continue;
 
       var srcPositions = (Vector2*)pool.Positions.GetUnsafeReadOnlyPtr();
       var srcRotations = (float*)pool.Rotations.GetUnsafeReadOnlyPtr();
